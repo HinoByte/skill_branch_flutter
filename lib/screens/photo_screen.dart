5c7fc615d0385c12bf3157910c7d81580c8b8435
Vladimir <znayka2005@gmail.com>
2020-05-07 17:28:30 +0300

    Lesson_3

--- lib/screens/photo_screen.dart
+++ lib/screens/photo_screen.dart
@@ -1,22 +1,39 @@
 import 'package:FlutterGalleryApp/res/res.dart';
+import 'package:FlutterGalleryApp/widgets/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FullScreenImage extends StatelessWidget {
-  FullScreenImage({Key key, this.photo}) : super(key: key);
+  FullScreenImage(
+      {this.altDescription, this.userName, this.name, this.photo, Key key})
+      : super(key: key);
 
-  final String photo;
+  final String altDescription, userName, name, photo;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.white,
-        title: Text('Photo', style: TextStyle(color: AppColors.black)),
-        centerTitle: true,
-        leading: IconButton(
-            icon: Icon(CupertinoIcons.back, color: AppColors.grayChateau),
-            onPressed: () {}),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: AppColors.white,
+          title: Text('Photo', style: TextStyle(color: AppColors.black)),
+          centerTitle: true,
+          leading: IconButton(
+              icon: Icon(CupertinoIcons.back, color: AppColors.grayChateau),
+              onPressed: () {}),
+        ),
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Photo(photoLink: photo),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: Text(
+                  altDescription,
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                  style: AppStyles.h3.copyWith(color: AppColors.manatee),
+                ),
+              )
+            ]));
   }
 }
