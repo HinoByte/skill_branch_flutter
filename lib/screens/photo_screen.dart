fdae51ad373a4c4971e7f1b8db163bf2eba1158d
Vladimir <znayka2005@gmail.com>
2020-05-22 02:47:45 +0300

    task_3

--- lib/screens/photo_screen.dart
+++ lib/screens/photo_screen.dart
@@ -5,10 +5,11 @@ import 'package:flutter/material.dart';
 
 class FullScreenImage extends StatelessWidget {
   FullScreenImage(
-      {this.altDescription = 'Beautiful girl in a yellow dress with a flower on her head in the summer in the forest',
+      {this.altDescription =
+          'Beautiful girl in a yellow dress with a flower on her head in the summer in the forest',
       this.userName = 'Dianne Miles',
       this.name = 'Dianne Miles',
-      this.photo = '',
+      this.photo = 'https://flutter.dev/assets/404/dash_nest-c64796b59b65042a2b40fae5764c13b7477a592db79eaf04c86298dcb75b78ea.png',
       Key key})
       : super(key: key);
 
@@ -32,23 +33,52 @@ class FullScreenImage extends StatelessWidget {
             ),
           ),
           _buildPhotoMeta(),
-          Container(
-            height: 8,
-          ),
           _buildRowButtons(),
         ],
       ),
     );
   }
 
-  Widget _buildRowButtons() {
+  Widget _buildPhotoMeta() {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
       child: Row(
+        children: <Widget>[
+          UserAvatar('https://skill-branch.ru/img/speakers/Adechenko.jpg'),
+          SizedBox(width: 6),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(name, style: AppStyles.h1Black),
+              Text('@' + userName,
+                  style: AppStyles.h5Black.copyWith(color: AppColors.manatee)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: AppColors.white,
+      title: Text('Photo', style: TextStyle(color: AppColors.black)),
+      centerTitle: true,
+      leading: IconButton(
+          icon: Icon(CupertinoIcons.back, color: AppColors.grayChateau),
+          onPressed: () => Navigator.pop(context)),
+    );
+  }
+
+  Widget _buildRowButtons() {
+    return Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Expanded(
-              child: Center(child: LikeButton(likeCount: 10, isLiked: true))),
+            child: Center(
+              child: LikeButton(likeCount: 10, isLiked: true),
+            ),
+          ),
           Expanded(
             child: _buildButton('Save'),
           ),
@@ -56,7 +86,7 @@ class FullScreenImage extends StatelessWidget {
             child: _buildButton('Visit'),
           ),
         ],
-      ),
+
     );
   }
 
@@ -84,37 +114,6 @@ class FullScreenImage extends StatelessWidget {
         ),
         margin: EdgeInsets.all(10.0),
       ),
-    );
-  }
-
-  Widget _buildPhotoMeta() {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Row(
-        children: <Widget>[
-          UserAvatar('https://skill-branch.ru/img/speakers/Adechenko.jpg'),
-          SizedBox(width: 6),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(name, style: AppStyles.h1Black),
-              Text('@$userName',
-                  style: AppStyles.h5Black.copyWith(color: AppColors.manatee)),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  AppBar _buildAppBar(BuildContext context) {
-    return AppBar(
-      backgroundColor: AppColors.white,
-      title: Text('Photo', style: TextStyle(color: AppColors.black)),
-      centerTitle: true,
-      leading: IconButton(
-          icon: Icon(CupertinoIcons.back, color: AppColors.grayChateau),
-          onPressed: () => Navigator.pop(context)),
     );
   }
 }
